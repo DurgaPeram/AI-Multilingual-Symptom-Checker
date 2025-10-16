071200c11d1b14c3a60b5a36efef747d8303b860
DurgaPeram <durgaperam2020@gmail.com>
2025-10-16 12:23:56 +0530

    third commit

--- pages/04_Help.py
+++ pages/04_Help.py
@@ -6,14 +6,15 @@ STYLES = BASE_DIR / "styles"
 STATIC = BASE_DIR / "static"
 st.markdown(f"<style>{(STYLES/'base.css').read_text()}</style>", unsafe_allow_html=True)
 # Header
-col1, col2, col3 = st.columns([1, 7, 1])
+col1, col2, col3 = st.columns([1, 6, 1])
 with col1:
-    st.image(str(STATIC/"images/icon.png"), width=130)
+    st.image(str(STATIC / "images/icon.png"), width=130)
 with col2:
-    st.markdown("<h1 style='color:#1976d2; font-size:2.4rem;'>WELCOME TO MULTILINGUAL SYMPTOM CHECKER</h1>", unsafe_allow_html=True)
-    st.markdown("<p style='color:#333; text-align: center;'>Get Preliminary Health Guidance based on your Symptoms in your preferred language</p>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:#1565c0;text-align:center;'> Welcome to Multiligual Symptom Checker</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align:center;color:#333;'>Analyze your symptoms using AI and get health insights in your preferred language.</p>", unsafe_allow_html=True)
 with col3:
-    st.image(str(STATIC/"images/medical.jpg"), width=130)
+    st.image(str(STATIC / "images/medical.jpg"), width=130)
+
 
 
 st.markdown("### How to Use")
